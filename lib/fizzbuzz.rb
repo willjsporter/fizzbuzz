e8f719ec34cd7875e4e07e4e439fbd82fb1bede5
FreddieCodes <FrederickCodes@gmail.com>
2017-07-26 17:58:30 +0000

    All tests passed

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(num)
-  if num==15
+  if num % 3 == 0 && num % 5 == 0
     "fizzbuzz"
-  elsif num%3==0
+  elsif num % 3 == 0
     "fizz"
-  elsif num%5==0
+  elsif num % 5 == 0
     "buzz"
   else num
   end
